a9d708c8b50939c5d866867721c8e0653e01a67c
Aaron <anhlt@twendeesoft.com>
2026-08-18 11:16:25 +0700

    update desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Cúp Hải Sản Twendee 2026-2027",
-  description: "Bảng phạt XP cho league FPL",
+  description: "Một cái đầu đầy sợ hãi không có chỗ cho những ước mơ!",
   icons: {
     icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🦑</text></svg>",
   },
